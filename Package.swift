20ef8b79ac6a310db58784d0e043a33a8a4af03c
AdsSDKMicrosoft@microsoft.com <Release Pipeline>
2026-07-13 21:52:26 +0000

    Release 9.12.2

--- Package.swift
+++ Package.swift
@@ -2,11 +2,11 @@
 
 import PackageDescription
 
-let sdkVersion = "9.12.1"
+let sdkVersion = "9.12.2"
 let baseUrl = "https://adsdk.bing.net/mobile/ios/releases"
 
 let sdkChecksum = """
-9de8a0bac5d31311c279bbd562bba19e4810cf77d22acc9606a9e59b66698929
+0c9abeaae6db78df5d0295d855528d05b8b7998e48455b0078df903aa957b2ed
 """
 
 let package = Package(
