13e5abb85cf67f1c953e7b871c81f01b5308c61a
ashamaz-ms <ashidov@microsoft.com>
2026-07-13 22:46:05 +0200

    beta-version

--- Package.swift
+++ Package.swift
@@ -1,58 +1,37 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
+let sdkVersion = "9.12.1"
+let baseUrl = "https://adsdk.bing.net/mobile/ios/releases"
+
+let sdkChecksum = """
+9de8a0bac5d31311c279bbd562bba19e4810cf77d22acc9606a9e59b66698929
+"""
+
 let package = Package(
     name: "AppNexusSDK",
+
     defaultLocalization: "en",
+
     platforms: [
         .iOS(.v12)
     ],
+
     products: [
         .library(
             name: "AppNexusSDK",
             targets: [
-                "OMSDK_Microsoft",
                 "AppNexusSDKTarget"
             ]
-        ),
-        .library(
-            name: "ANGoogleAdapter",
-            targets: [
-               "ANGoogleMobileAdsTarget"
-            ]
-        ),
+        )
     ],
-     dependencies: [
-        .package(name: "GoogleMobileAds", url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", .exact("12.8.0"))
-    ],
+
     targets: [
         .binaryTarget(
-            name: "OMSDK_Microsoft",
-            url:
-                "https://adsdk.bing.net/mobile/ios/releases/9.13.0/dynamic/OMSDK_Microsoft.zip",
-            checksum: "8349cb2d6afb530f7b940ec1105005190ef0fec1375c5f8e4bb8580da46e9500"
-        ),
-         .binaryTarget(
             name: "AppNexusSDKTarget",
-            url:
-                "https://adsdk.bing.net/mobile/ios/releases/9.13.0/dynamic/AppNexusSDKDynamic.zip",
-            checksum: "e6e38cfc5aeb3d4fbe6a6a8f190283f48a3e0779448dd78fc36394fd5a0f5aeb"
-        ),
-      .target(
-         name: "ANGoogleMobileAdsTarget",
-      dependencies: [
-        .target(name: "ANGoogleMobileAds"),
-        .target(name: "AppNexusSDKTarget"),
-        .product(name: "GoogleMobileAds", package: "GoogleMobileAds")
-      ],
-      path: "ANGoogleMobileAdsTarget"
-    ),
-    .binaryTarget(
-      name: "ANGoogleMobileAds",
-      url: "https://adsdk.bing.net/mobile/ios/releases/9.13.0/static/ANGoogleAdapter.zip",
-     checksum: "02943aec470670f5e0cd5c277bf72fa3db3db8064f5136d453b3bd6a256c5d84"
-    )
+            url: "\(baseUrl)/\(sdkVersion)/static/AppNexusSDK.zip",
+            checksum: sdkChecksum
+        )
     ]
 )
